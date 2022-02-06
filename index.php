9f752c67daed693802e2ae05ced705162e88c50f
TechzQuad <75603660+TechzQuad@users.noreply.github.com>
2022-02-06 18:38:53 +0800

    Update index.php
    in

--- index.php
+++ index.php
@@ -1,44 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+  <?php include'incl/links.php';?>
 </head>
 <body>
 
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Toboso Election Vote Casting App</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="collapsibleNavbar">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Home</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Settings</a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Settings1</a></li>
-            <li><a class="dropdown-item" href="#">Settings2</a></li>
-            <li><a class="dropdown-item" href="#">Settings3</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+<?php include'incl/nav.php';?>
 
 <div class="container mt-3">
   <h1>TechzQuad Welcome Aboard!</h1>
  
 </div>
-<div class="alert alert-dark container">
-    <strong>Developed By TechzQuad</strong> | We Are The Solution
-  </div>
+<?php include'incl/links.php';?>
 </body>
 </html>
