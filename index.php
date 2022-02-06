98c0ddf22af006b482263f09ca0f25aa387ec36d
TechzQuad <75603660+TechzQuad@users.noreply.github.com>
2022-02-06 19:07:16 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,28 +9,17 @@
 
 <div class="container-fluid mt-3">
   <h1>Municipality of Toboso May 2022 Election Result</h1>
-  <div class="row">
-    <div class="col-md-12">
-      <div class="col-md-6">
-        <div class="card bg-info text-white">
-          <div class="card-body">Info card</div>
-        </div>
-      </div>
-      <div class="col-md-6">
-        <div class="card bg-info text-white">
-          <div class="card-body">Info card</div>
-        </div>
-      </div>
-  </div>  
-</div>
+ 
 <div class="row">
     <div class="col-sm-6">
     <div class="card bg-info text-white">
-          <div class="card-body">Info card</div>
+        <div class="card-body">Info card</div>
         </div>
     </div>
-    <div class="col-sm-6 bg-dark text-white p-3">
-      Sed ut perspiciatis...
+    <div class="col-sm-6">
+    <div class="card bg-info text-white">
+        <div class="card-body">Info card</div>
+        </div>
     </div>
   </div>
 </div>
