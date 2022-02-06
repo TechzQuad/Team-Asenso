ca0ae7bb654e6f85c206b61c04c91f9a74366b96
TechzQuad <75603660+TechzQuad@users.noreply.github.com>
2022-02-06 18:44:21 +0800

    Update links.php

--- incl/links.php
+++ incl/links.php
@@ -1,4 +1,4 @@
-<title>Bootstrap Example</title>
+<title>Toboso Vote Casting App</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
