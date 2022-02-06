457c1c4981836c89dba99a7730ea7426d7180b69
TechzQuad <75603660+TechzQuad@users.noreply.github.com>
2022-02-06 10:38:38 +0800

    sd
    sds

--- incl/nav.php
+++ incl/nav.php
@@ -10,7 +10,7 @@
           <a class="nav-link" href="https://onetechzquad.tech">Dashboard</a>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Voters</a>
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Voters List</a>
           <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="https://onetechzquad.tech/cluster-leaders.php">Cluster Leaders</a></li>
             <li><a class="dropdown-item" href="https://onetechzquad.tech/coordinators.php">Coordinators</a></li>
