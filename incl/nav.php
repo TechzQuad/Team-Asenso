0afe75357215b6f252fb8cf8f38091f1188ab004
TechzQuad <75603660+TechzQuad@users.noreply.github.com>
2022-02-06 10:35:20 +0800

    overall
    hanot

--- incl/nav.php
+++ incl/nav.php
@@ -1,20 +1,21 @@
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Toboso Election Vote Casting App</a>
+    <a class="navbar-brand" href="https://onetechzquad.tech">Toboso Election Vote Casting App</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="collapsibleNavbar">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="#">Home</a>
+          <a class="nav-link" href="https://onetechzquad.tech">Dashboard</a>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Settings</a>
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Voters</a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Settings1</a></li>
-            <li><a class="dropdown-item" href="#">Settings2</a></li>
-            <li><a class="dropdown-item" href="#">Settings3</a></li>
+            <li><a class="dropdown-item" href="https://onetechzquad.tech/cluster-leaders.php">Cluster Leaders</a></li>
+            <li><a class="dropdown-item" href="https://onetechzquad.tech/coordinators.php">Coordinators</a></li>
+            <li><a class="dropdown-item" href="https://onetechzquad.tech/leaders.php">Leaders</a></li>
+            <li><a class="dropdown-item" href="https://onetechzquad.tech/cluster-overall.php">Overall</a></li>
           </ul>
         </li>
       </ul>
