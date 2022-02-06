f1ab2c5f8703a1216cec818a363e5b8f114417af
TechzQuad <75603660+TechzQuad@users.noreply.github.com>
2022-02-06 19:29:52 +0800

    Update cluster-leaders.php

--- cluster-leaders.php
+++ cluster-leaders.php
@@ -9,7 +9,27 @@
 
 <div class="container mt-3">
   <h1>Cluster Leaders Table</h1>
- 
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Address</th>
+        <th>Precinct #</th>
+        <th>Position</th>
+        <th>Status</th>
+        <th><a href="View"></a></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Mark Corral Banilarskie</td>
+        <td>Bandila Proper</td>
+        <td>220A</td>
+        <td>Leader</td>
+        <td>Voted</td>
+      </tr>
+    </tbody>
+  </table>
 </div>
 <?php include'incl/footer.php';?>
 </body>
